06563ea00a5c05318fdf29bc6ae4a5c1e52847e2
nikolay.novik <nikolay.novik@stoloto.ru>
2019-07-19 14:56:55 +0300

    Разное

---
+++ project10/src/CheckVariable.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CheckVariable {
+  public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+
+	  boolean b = false;
+    if (b = true) System.out.println("b is true");
+  }
+}
